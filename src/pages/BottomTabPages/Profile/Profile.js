1bd70131722900b5b44fca7b6fd6c05325aea409
Erhan <39314007+ErhanAlgl@users.noreply.github.com>
2024-05-12 23:35:14 +0300

    Update Profile.js

--- src/pages/BottomTabPages/Profile/Profile.js
+++ src/pages/BottomTabPages/Profile/Profile.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {SafeAreaView, Button, Text} from 'react-native';
 import {useDispatch} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
+import {useSurveyContext} from '../../../SurveyContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = () => {
   const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const {username} = useSurveyContext();
 
   const handleLogOut = () => {
     dispatch({type: 'REMOVE_USER'});
+    AsyncStorage.removeItem('username');
+    navigation.navigate('Pages');
   };
 
   return (
     <SafeAreaView>
-      <Text>Çıkış Yap</Text>
+      <Text>Güle Güle, John Doe{username}</Text>
       <Button title="Log Out" onPress={handleLogOut} />
     </SafeAreaView>
   );
